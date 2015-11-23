a4df770097f6e2b6a637bef66540b2541c2ed350
Erik Erice <erik@redmonster.es>
2015-11-23 22:22:56 +0100

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Erik Sebastian de Erice on 23/11/15.
+//  Copyright © 2015 Red Monster Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
